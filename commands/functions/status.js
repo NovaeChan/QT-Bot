17d8c105c4f99bc705ca9fd41f9d4d04e4c6790b
NekoElise <novaestar@protonmail.com>
2021-04-16 18:43:37 +0200

    Updated repo

--- commands/functions/status.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = (presenceStatus) => {
-    return presenceStatus === "offline" ? ":black_circle: - Offline"
-        :  presenceStatus === "online"  ? ":green_circle: - Online"
-        :  presenceStatus === "idle"    ? ":yellow_circle: - Idle"
-        :                                 ":red_circle: - Do Not Disturb"
-}
--- commands/help.js
+++
@@ -1,3 +0,0 @@
-module.exports = async (msg, args) => {
-    msg.channel.send("Flemme de faire les commandes pour le moment. Le bot n'est pas totalement fini alors go @ Novae (Oui je parle en français là)");
-}
